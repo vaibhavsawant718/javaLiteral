90fdd9ae2b96fcd576c7cdf300b416aff4e98d30
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-09 21:37:23 +0530

    Boolean Literal

---
+++ BooleanLiteral.java
@@ -0,0 +1,18 @@
+class booleanliteral
+{
+	public static void main(String[]args)
+	{
+		
+		boolean b=true;
+		boolean b1=false;
+		//boolean b2=0;		// error: incompatible types: int cannot be converted to boolean
+		//boolean b3=True;       // error: cannot find symbol
+		//boolean b4="True";	// error: incompatible types: String cannot be converted to boolean
+		System.out.println(b);
+		System.out.println(b1);
+		//System.out.println(b2);
+		//System.out.println(b3);
+		//System.out.println(b4);
+		
+	}
+}
