efab5f3de8bd78f6fee6af99a22fa55607e9a385
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-09 21:48:46 +0530

    Float Literal

---
+++ floatingliteral2.java
@@ -0,0 +1,17 @@
+class floatliteral2
+{
+	public static void main(String[]args)
+	{
+		
+		 
+		//float f3=123.4567D; 	 //error: incompatible types: possible lossy conversion from double to float
+		double d=123.456;
+		double d1=01323.456;
+		//double d2=0x123.456;   //error: malformed floating-point literal
+		//System.out.println(f3); 
+		System.out.println(d);
+		System.out.println(d1);
+		//System.out.println(d2);
+		
+	}
+}
