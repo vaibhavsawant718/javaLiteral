ddc251c27ec77f1fe43efc0e59933cb08580a0aa
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-13 20:38:10 +0530

    char_literal3

---
+++ specialcase.java
@@ -0,0 +1,13 @@
+
+class test1
+{
+	public static void main(String[]args)
+	{
+		char ch1='\n'; 
+		char ch2='\t';
+		char ch3='\l';
+		System.out.println(ch1); 
+		System.out.println(ch2);
+		System.out.println(ch3);
+	}
+}
