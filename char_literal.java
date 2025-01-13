4d190907875383b21027e53e5d07a3196c091236
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-13 20:23:54 +0530

    char_literal

---
+++ char_literal.java
@@ -0,0 +1,14 @@
+class char1
+{
+	public static void main(String[]args)
+	{
+		char ch='a';
+ 		 //char ch1=a;  //Invalid 
+  		//char ch2="a";
+ 		 //char ch3='ab';
+		System.out.println(ch);
+		//System.out.println(ch1);
+		//System.out.println(ch2);
+		//System.out.println(ch3);
+	}
+}
